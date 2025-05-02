b5ff65795137d37939a4d5fb773a66e2e67d1b8a
eduardopolanc <eduardoantonio.polancoarrindell@epfedu.fr>
2025-05-02 19:14:13 +0200

    creating new files for pages

--- exe.py
+++
@@ -1,9 +0,0 @@
-import sys
-from streamlit import cli as stcli 
-
-# Cette fonction permet d'appeler le scritp app.py pour executer les streamlit
-
-if __name__ == '__main__': 
-    sys.argv = ["streamlit", "run", "app.py"]
-    sys.exit(stcli.main())
-
