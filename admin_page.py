c7f3f3d6d2fa688f7a389a25b02e8313a36d203a
eduardo <eduardo@eduardoantonio.polancoarrindell@epfedu.fr>
2025-05-12 21:35:45 +0200

    trying notification in admin page

--- admin_page.py
+++ admin_page.py
@@ -1,6 +1,24 @@
 import streamlit as st
+import os
+import pandas as pd
 
 def admin_page():
+    request_file = "demandes_en_attente.xlsx"
+    pending_count = 0
+    if os.path.exists(request_file):
+        try:
+            requests = pd.read_excel(request_file)
+            pending_count = len(requests)
+        except:
+            pass
+    
+    col1, col2 = st.columns([6, 1])
+    with col2:
+        if pending_count > 0:
+            if st.button(f"🔔 {pending_count} solicitudes"):
+                st.session_state.page = "request"
+                st.rerun()
+
     button_user = st.button("user page")
     button_pdf = st.button("pdf page")
     button_out = st.button("log out")
