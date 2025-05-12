3ed3a0f2e8c1315fc5bdb936d2679f99654beed6
eduardo <eduardo@eduardoantonio.polancoarrindell@epfedu.fr>
2025-05-12 21:45:52 +0200

    adsada

--- admin_page.py
+++ admin_page.py
@@ -3,7 +3,6 @@ import os
 import pandas as pd
 
 def admin_page():
-    # Leer solicitudes pendientes
     request_file = "demandes_en_attente.xlsx"
     pending_count = 0
     if os.path.exists(request_file):
@@ -12,23 +11,11 @@ def admin_page():
             pending_count = len(requests)
         except:
             pass
-
-    # Notificación en la parte superior derecha
+    
     col1, col2 = st.columns([6, 1])
     with col2:
         if pending_count > 0:
-            notif_button = f"""
-            <div style='text-align: right;'>
-                <form action='#' method='post'>
-                    <button style='font-size:12px; padding:5px 10px; border-radius:5px; background-color:#444; color:white; border:none;'>
-                        🔔 {pending_count} demande{'s' if pending_count > 1 else ''}
-                    </button>
-                </form>
-            </div>
-            """
-            clicked = st.markdown(f"""<a href="#" onclick="window.location.reload();">{notif_button}</a>""", unsafe_allow_html=True)
-            # simulamos navegación al hacer clic con un botón invisible
-            if st.button("Go to demandes", key="invisible"):
+            if st.button(f"🔔 {pending_count}"):
                 st.session_state.page = "request"
                 st.rerun()
 
