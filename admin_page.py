a358a033d1242dd18fe3c3cdccf8acd1068ae884
eduardo <eduardo@eduardoantonio.polancoarrindell@epfedu.fr>
2025-05-24 20:09:39 +0200

    sadas

--- admin_page.py
+++ admin_page.py
@@ -73,7 +73,7 @@ def admin_page():
                     ">
                         {links_html}
                     </div>
-                """, height=120)
+                """, height=150)
         else:
             st.warning("Le dossier des PDF n'existe pas.")
 
