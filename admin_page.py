0d0ac83f9a0261522477a7c81633056c2de9be1a
eduardo <eduardo@eduardoantonio.polancoarrindell@epfedu.fr>
2025-06-10 23:54:23 +0200

    changing layout of admin page

--- admin_page.py
+++ admin_page.py
@@ -161,9 +161,13 @@ def admin_page():
 
                     with col3:
                         if st.button("👁️ Voir", key=f"voir_{filename}"):
-                            st.session_state.page = "viewer"
-                            st.session_state.pdf_to_view = filename
-                            st.rerun()
+                            pdf_url = f"/app/static/pdf_reports/{urllib.parse.quote(filename)}"
+                            js = f"""
+                                <script>
+                                window.open("{pdf_url}", "_blank").focus();
+                                </script>
+                            """
+                            st.markdown(js, unsafe_allow_html=True)
                     st.markdown('<hr style="margin: 6px 0;">', unsafe_allow_html=True)
 
     if st.button("Generer un PDF"):
