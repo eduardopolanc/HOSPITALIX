54b75ac9995778383fb0670e9ab5cc00df468b9e
eduardo <eduardo@eduardoantonio.polancoarrindell@epfedu.fr>
2025-06-10 22:08:45 +0200

    changing layout of admin page

--- admin_page.py
+++ admin_page.py
@@ -143,11 +143,11 @@ def admin_page():
         else:
             with st.container(height=420):
                 for filename in pdf_files[:50]:
-                    st.markdown("---")
+                    st.markdown('<hr style="margin: 6px 0;">', unsafe_allow_html=True)
                     col1, col2, col3 = st.columns([3, 1, 1])
 
                     with col1:
-                        st.markdown(f"📄 **{filename}**")
+                        st.markdown(f"<div style='font-size: 14px; margin: 0; padding: 0;'>📄 <b>{filename}</b></div>", unsafe_allow_html=True)
                     
                     with col2:
                         file_path = os.path.join(pdf_folder, filename)
@@ -159,7 +159,7 @@ def admin_page():
                             st.session_state.page = "viewer"
                             st.session_state.pdf_to_view = filename
                             st.rerun()
-                    st.markdown("---")
+                    st.markdown('<hr style="margin: 6px 0;">', unsafe_allow_html=True)
 
     if st.button("Generer un PDF"):
         st.session_state.page = "user"
