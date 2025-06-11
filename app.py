c16f5fc81560fd62bd47f16d5d7531d07098a283
eduardo <eduardo@eduardoantonio.polancoarrindell@epfedu.fr>
2025-06-11 15:27:46 +0200

    columns

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 import streamlit as st
 from admin_page import admin_page
 from login_page import login_page
-from pdf_viewer_page import pdf_viewer_page
 from sign_up_page import sign_up_page
 from user_page import user_page
-from review_request_page import review_request_page
 
 # Session state defaults
 if 'page' not in st.session_state:
@@ -19,14 +17,10 @@ if page == "login":
     login_page()
 elif page == "admin":
     admin_page()
-elif page == "viewer":
-    pdf_viewer_page()
 elif page == "signup":
     sign_up_page()
 elif page == "user":
     user_page()
-elif page in ["review", "request"]:
-    review_request_page()
 else:
     st.warning("Page not authorized. Redirecting to Login...")
     st.session_state.page = 'login'
