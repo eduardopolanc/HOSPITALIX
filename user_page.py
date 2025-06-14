042fd9d1bab0634b50e554f4c8258a89f0c82784
eduardo <eduardo@eduardoantonio.polancoarrindell@epfedu.fr>
2025-06-14 14:17:47 +0200

    comments

--- user_page.py
+++ user_page.py
@@ -233,6 +233,7 @@ def user_page():
         df.to_excel(commentaire_path, index=False, engine="openpyxl")
         st.success("✅ Commentaire envoyé avec succès.")
         st.session_state.comment_text = ""
+        st.session_state.comment_submitted = True
         st.rerun()
 
     # Dialogue de confirmation
@@ -244,7 +245,7 @@ def user_page():
         with col1:
             if st.button("✅ Oui, envoyer", key="confirm_envoyer"):
                 enregistrer_commentaire(texte)
-                st.rerun()
+
         with col2:
             if st.button("❌ Annuler", key="cancel_envoyer"):
                 st.rerun()
@@ -259,6 +260,9 @@ def user_page():
     else:
         if "comment_text" not in st.session_state:
             st.session_state.comment_text = ""
+
+        if "comment_submitted" in st.session_state:
+            del st.session_state.comment_submitted
 
         txt = st.text_area(
             "Votre commentaire :",
