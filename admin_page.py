6572c525944a2eb6b013c106aefee4d2e53ce8ae
eduardo <eduardo@eduardoantonio.polancoarrindell@epfedu.fr>
2025-06-17 01:07:23 +0200

    deleting pdf view

--- admin_page.py
+++ admin_page.py
@@ -194,7 +194,7 @@ def admin_page():
         if "selected_user_idx" not in st.session_state:
             st.session_state.selected_user_idx = None
 
-        with st.container(height=400):
+        with st.container(height=450):
             if filtered_requests.empty:
                 st.info("Aucune demande trouvée.")
             else:
@@ -307,7 +307,7 @@ def admin_page():
         if search_email:
             actifs = actifs[actifs['Email (username)'].str.lower().str.contains(search_email)]
 
-        with st.container(height=400):
+        with st.container(height=450):
             if actifs.empty:
                 st.info("Aucun utilisateur trouvé.")
             else:
@@ -360,7 +360,7 @@ def admin_page():
         if search_email:
             supprimes = supprimes[supprimes['Email (username)'].str.lower().str.contains(search_email)]
 
-        with st.container(height=400):
+        with st.container(height=450):
             if supprimes.empty:
                 st.info("Aucun utilisateur supprimé.")
             else:
