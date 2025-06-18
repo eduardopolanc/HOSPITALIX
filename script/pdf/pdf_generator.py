462b29cc34bfef7a584a3f342dab7a118063b5da
eduardo <eduardo@eduardoantonio.polancoarrindell@epfedu.fr>
2025-06-18 23:22:18 +0200

    sadas

--- script/pdf/pdf_generator.py
+++ script/pdf/pdf_generator.py
@@ -3,7 +3,7 @@ from script.pdf.Clean_scritp import clean_scritp
 from pathlib import Path
 
 IMAGE_DIR = Path(__file__).resolve().parent / "image"
-FONT_PATH = Path("/srv/HOSPITALIX/script/pdf/DejaVuSansCondensed.ttf")
+FONT_PATH = Path(__file__).resolve().parent / "DejaVuSansCondensed.ttf"
 
 class PDF(FPDF):
     def header(self):
