4528d76ecb97325e152efd3611e0472cd884a8d1
eduardo <eduardo@eduardoantonio.polancoarrindell@epfedu.fr>
2025-06-19 17:01:37 +0200

    asdas

--- user_page.py
+++ user_page.py
@@ -90,7 +90,7 @@ def user_page():
             return None
         try:
             df = pd.read_excel(USER_FILE, engine="openpyxl")
-            if not all(col in df.columns for col in ["Email (username)", "Password"]):
+            if not all(col in df.columns for col in ["Email (username)", "Hashed Password"]):
                 return None
             return df
         except:
