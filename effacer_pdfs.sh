3962d7d5936de74e6cc4fee9f05d670ca57685f2
eduardopolanc <eduardoantonio.polancoarrindell@epfedu.fr>
2025-06-19 10:56:35 +0200

    last commit

--- effacer_pdfs.sh
+++ effacer_pdfs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Supprimer tous les fichiers .pdf modifiés il y a plus de 30 jours
-find "/data/copie_windows/version Windows alix02/Desktop/ALIX_APP_DEV/static" -name "*.pdf" -type f -mtime +30 -exec rm {} \;
+find "/srv/HOSPITALIX/static" -name "*.pdf" -type f -mtime +30 -exec rm {} \;
